fdd52c17b007425f67b648c1ba24f853d4e0497e
Ahmad Fashich Azzuhri Ramadhani <thalassaaddict@gmail.com>
2026-07-01 07:55:33 +0700

    feat: add render production config

--- backend/boilerplate/config.php
+++ backend/boilerplate/config.php
@@ -6,26 +6,27 @@ use Rajasa\PresensiSiswa\Support\Env;
 
 return [
     'app' => [
-        'name' => Env::get('APP_NAME', 'Presensi Siswa Rajasa'),
-        'env' => Env::get('APP_ENV', 'local'),
-        'debug' => Env::bool('APP_DEBUG', true),
-        'url' => Env::get('APP_URL', 'http://localhost:8080'),
+        'name'     => Env::get('APP_NAME', 'Presensi Siswa Rajasa'),
+        'env'      => Env::get('APP_ENV', 'local'),
+        'debug'    => Env::bool('APP_DEBUG', true),
+        'url'      => Env::get('APP_URL', 'http://localhost:8080'),
         'timezone' => Env::get('APP_TIMEZONE', 'Asia/Jakarta'),
     ],
 
     'database' => [
-        'driver' => Env::get('DB_CONNECTION', 'mysql'),
-        'host' => Env::get('DB_HOST', 'db'),
-        'port' => Env::get('DB_PORT', '3306'),
-        'database' => Env::get('DB_DATABASE', 'sistem_presensi_siswa_qr'),
-        'username' => Env::get('DB_USERNAME', 'root'),
-        'password' => Env::get('DB_PASSWORD', ''),
-        'charset' => 'utf8mb4',
+        'driver'    => Env::get('DB_CONNECTION', 'mysql'),
+        'host'      => Env::get('DB_HOST', 'db'),
+        'port'      => Env::get('DB_PORT', '3306'),
+        'database'  => Env::get('DB_DATABASE', 'sistem_presensi_siswa_qr'),
+        'username'  => Env::get('DB_USERNAME', 'root'),
+        'password'  => Env::get('DB_PASSWORD', ''),
+        'charset'   => 'utf8mb4',
         'collation' => 'utf8mb4_unicode_ci',
+        'ssl'       => Env::bool('DB_SSL', false), // aktifkan untuk TiDB Cloud
     ],
 
     'auth' => [
-        'secret' => Env::get('SESSION_SECRET', 'change_me_for_local_dev'),
+        'secret'            => Env::get('SESSION_SECRET', 'change_me_for_local_dev'),
         'token_ttl_minutes' => Env::int('ACCESS_TOKEN_TTL_MINUTES', 720),
     ],
 
@@ -37,6 +38,6 @@ return [
 
     'presensi' => [
         'session_timeout_minutes' => Env::int('PRESENSI_SESSION_TIMEOUT_MINUTES', 20),
-        'max_jam_per_sesi' => Env::int('PRESENSI_MAX_JAM_PER_SESI', 3),
+        'max_jam_per_sesi'        => Env::int('PRESENSI_MAX_JAM_PER_SESI', 3),
     ],
 ];
--- backend/boilerplate/database.php
+++ backend/boilerplate/database.php
@@ -8,16 +8,23 @@ use Rajasa\PresensiSiswa\Support\Config;
 return function (): Capsule {
     $capsule = new Capsule();
 
+    // SSL dibutuhkan untuk TiDB Cloud Serverless
+    $options = [];
+    if (Config::get('database.ssl', false)) {
+        $options[PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT] = false;
+    }
+
     $capsule->addConnection([
-        'driver' => Config::get('database.driver', 'mysql'),
-        'host' => Config::get('database.host', 'db'),
-        'port' => Config::get('database.port', '3306'),
-        'database' => Config::get('database.database'),
-        'username' => Config::get('database.username'),
-        'password' => Config::get('database.password'),
-        'charset' => Config::get('database.charset', 'utf8mb4'),
+        'driver'    => Config::get('database.driver', 'mysql'),
+        'host'      => Config::get('database.host', 'db'),
+        'port'      => Config::get('database.port', '3306'),
+        'database'  => Config::get('database.database'),
+        'username'  => Config::get('database.username'),
+        'password'  => Config::get('database.password'),
+        'charset'   => Config::get('database.charset', 'utf8mb4'),
         'collation' => Config::get('database.collation', 'utf8mb4_unicode_ci'),
-        'prefix' => '',
+        'prefix'    => '',
+        'options'   => $options,
     ]);
 
     $capsule->setAsGlobal();
